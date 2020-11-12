a161ea9108acef36ebf30945ee84b848177bd28e
d.ts <samudra3004@gmail.com>
2020-11-12 19:45:09 +0700

    data persistence: done

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,28 +12,11 @@ export class TasksService {
         @InjectRepository(TaskRepository)
         private taskRepository: TaskRepository,
     ) {}
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
 
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(task =>
-  //       task.title.includes(search) ||
-  //       task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
-
+    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+      return this.taskRepository.getTasks(filterDto);
+    }
+  
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepository.findOne(id);
 
@@ -48,14 +31,18 @@ export class TasksService {
         return this.taskRepository.createTask(createTaskDto);
     }
 
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskById(id);
-  //   this.tasks = this.tasks.filter(task => task.id !== found.id);
-  // }
+    async deleteTask(id: number): Promise<void> {
+      const result = await this.taskRepository.delete(id);
+      
+      if (result.affected ===0) {
+        throw new NotFoundException(`Task with ID "${id}" not found`);
+      }
+    }
 
-  // updateTaskStatus(id: string, status: TaskStatus): Task {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
+    async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+      const task = await this.getTaskById(id);
+      task.status = status;
+      await task.save();
+      return task;
+    }  
 }
